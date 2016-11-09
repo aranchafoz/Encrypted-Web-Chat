b0259a300401c786ef5dc28f8d833334d04df162
Enrique Mas Candela <enriquemc96@gmail.com>
2016-11-09 21:09:00 +0800

    Added names of each username in the message

---
+++ public/client.js
@@ -0,0 +1,40 @@
+var username = prompt("Please enter your name");
+var key = prompt("Enter the encryption key");
+
+var lastSender = "";
+
+function parsePackage(pkg) {
+    var spl = pkg.split('%%%%');
+    return { user: spl[0], message: spl[1] };
+}
+
+function encrypt(message) {
+  return message;
+}
+
+function decrypt(message) {
+  return message;
+}
+
+var socket = io();
+
+$('form').submit(function(){
+  socket.emit('chat message', encrypt(username + '%%%%' + $('#m').val()));
+  $('#m').val('');
+  return false;
+});
+
+socket.on('chat message', function(pkg){
+  pkg = parsePackage(decrypt(pkg));
+
+  var messageTitle = "";
+
+  if (lastSender != pkg["user"]) {
+    messageTitle = '<p class="username">' + pkg["user"] + '</p>';
+    lastSender = pkg["user"];
+  }
+  let messageContent = '<p class="message_content">' + pkg["message"] + '</p>';
+
+  $('#messages').append(
+    $('<li id="' + pkg["user"] + '">').append(messageTitle).append(messageContent));
+});
